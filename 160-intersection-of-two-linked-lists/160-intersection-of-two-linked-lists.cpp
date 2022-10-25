38741e24f54e5e10ea89fdc724e92e6fa2ae571a
Shrey <thisisshreykhandelwal@protonmail.com>
2022-10-26 00:57:54 +0530

    Time: 117 ms (28.12%), Space: 14.3 MB (98.85%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -9,12 +9,22 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode *tempa = headA;
-        ListNode *tempb = headB;
-        while (tempa != tempb){
-            tempa = tempa != NULL ? tempa->next : headB;
-            tempb = tempb != NULL ? tempb->next : headA;
-        }
-        return tempa;
+        ListNode *ptr1 = headA;
+		ListNode *ptr2 = headB;
+		while(ptr1 != ptr2){
+			if(ptr1 == NULL){
+				ptr1 = headB;
+			}
+			else{
+				ptr1 = ptr1 -> next;
+			}
+			if(ptr2 == NULL){
+				ptr2 = headA;
+			}
+			else{
+				ptr2 = ptr2 -> next;
+			}
+		}
+		return ptr1;
     }
 };
