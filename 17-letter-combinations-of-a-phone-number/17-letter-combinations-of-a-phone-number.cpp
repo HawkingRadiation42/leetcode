5b5159526a7c1e7325d698b3058856f8a8fdf1fb
Shrey <thisisshreykhandelwal@protonmail.com>
2023-01-12 04:02:34 +0530

    Time: 4 ms (33.34%), Space: 6.4 MB (94.35%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-    map<int, string> m;
-    vector<string> ans;
     vector<string> letterCombinations(string digits) {
-        if(digits=="")
+        vector<string> ans;
+        if(digits.length()==0){
             return ans;
-        m[2] = "abc";
-        m[3] = "def";
-        m[4] = "ghi";
-        m[5] = "jkl";
-        m[6] = "mno";
-        m[7] = "pqrs";
-        m[8] = "tuv";
-        m[9] = "wxyz";
-        
-        string currstring = "";
-        makecombinations(digits, currstring, 0);
+        }
+        string output;
+        int index = 0;
+        string mapping[10]={"", "","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        solve(digits, output, index, ans, mapping);
         return ans;
     }
-    void makecombinations(string digits, string &currstring, int i){
-        if(i==digits.size()){
-            ans.push_back(currstring);
+    
+    void solve(string digits, string output, int index, vector<string> &ans, string mapping[]){
+        
+        //base case
+        if(index>=digits.length()){
+            ans.push_back(output);
             return;
         }
-        for(auto letter : m[digits[i]-'0']){
-            currstring.push_back(letter);
-            makecombinations(digits, currstring, i+1);
-            currstring.pop_back();
+        
+        int number = digits[index]-'0';
+        string value = mapping[number];
+        
+        for(int i=0;i<value.length();i++){
+            output.push_back(value[i]);
+            solve(digits, output, index+1, ans, mapping);
+            output.pop_back();
         }
-        return; 
     }
-    
 };
