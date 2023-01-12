b13bca6c4f6160f038a69d7025dc9adcfe5515a1
Shrey <thisisshreykhandelwal@protonmail.com>
2023-01-12 18:08:06 +0530

    Time: 15 ms (95.41%), Space: 16.8 MB (98.85%) - LeetHub

---
+++ 136-single-number/136-single-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        if(nums.size()<2){
+            return nums[0];
+        }
+        int ans =0;
+        for(auto x:nums){
+            ans^=x;
+        }
+        return ans;
+    }
+};
