b56051cda25d886267a98c08be752e87e186a8eb
Shrey <thisisshreykhandelwal@protonmail.com>
2023-01-14 16:50:49 +0530

    Time: 31 ms (97.79%), Space: 12.3 MB (87.17%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -2,13 +2,15 @@ class Solution {
 public:
     
     void DFS(vector<vector<char>> &grid, int i, int j){
-        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size())    
+        
+        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size()) //boundary checking   
             return;
         
-        if(grid[i][j]=='2' || grid[i][j]=='0')
+        if(grid[i][j]=='2' || grid[i][j]=='0')//return if current position is water or visited when used in recursion in the below code it might land in water again while checking
             return;
-        grid[i][j]='2';
-        
+
+        grid[i][j]='2';//mark the current as visited 
+        //do dfs in all 4 directions 
         DFS(grid, i+1, j);
         DFS(grid, i, j+1);
         DFS(grid, i, j-1);
@@ -18,7 +20,7 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         if (grid.size()==NULL)
             return 0;
-        
+//each island is a connected component
         int ans =0;
         
         for(int i=0;i<grid.size();i++){
