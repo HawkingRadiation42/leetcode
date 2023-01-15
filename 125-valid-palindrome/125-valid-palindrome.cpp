712bf61fc795137fdc1dd5634465afd7b96ab5cb
Shrey <thisisshreykhandelwal@protonmail.com>
2023-01-15 19:08:26 +0530

    Time: 4 ms (82.98%), Space: 7.3 MB (81.83%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -5,13 +5,13 @@ public:
         int r=s.size()-1;
         
         while(l<r){
-            if(!isalnum(s[l])){
+            if(!isalnum(s[l])){ //isalnum is used to check if the character is alpha numeric or not
                 l++;
             }
             else if(!isalnum(s[r])){
                 r--;
             }
-            else if(tolower(s[l])!=tolower(s[r])){
+            else if(tolower(s[l])!=tolower(s[r])){ //to lower the characters as there are capital letters present too
                 return false;
             }
             else{
