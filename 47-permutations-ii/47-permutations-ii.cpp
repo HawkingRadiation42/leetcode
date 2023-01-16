90465f35d06ede6bd2c8cebb1d630bd97498fa75
Shrey <thisisshreykhandelwal@protonmail.com>
2023-01-16 10:57:53 +0530

    Time: 4 ms (93.49%), Space: 8.5 MB (89.20%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -4,7 +4,7 @@ public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<vector<int>> ans;
         sort(begin(nums), end(nums));
-        
+        //not a perfect solution as it uses existing function
         ans.emplace_back(nums);
         while (next_permutation(begin(nums), end(nums))) {
             ans.emplace_back(nums);
