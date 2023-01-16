03c53fd94b598e93ab9d0cbcb15e26db290f8398
Shrey <thisisshreykhandelwal@protonmail.com>
2023-01-16 09:44:24 +0530

    Time: 20 ms (81.22%), Space: 19.6 MB (66.77%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> map;
-        for(auto i: nums){
-            map[i]++;
-        }
-        int maxval = nums[0];
-        for(auto it:map){
-            if(it.second>map[maxval]){
-                maxval = it.first;
-            }
-        }
-        return maxval;
+        // unordered_map<int, int> map;
+        // for(auto i: nums){
+        //     map[i]++;
+        // }
+        // int maxval = nums[0];
+        // for(auto it:map){
+        //     if(it.second>map[maxval]){
+        //         maxval = it.first;
+        //     }
+        // }
+        // return maxval;
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        return nums[n/2];
     }
+    
 };
