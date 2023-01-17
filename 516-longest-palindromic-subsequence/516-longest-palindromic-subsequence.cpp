fd005819eeb6258bbdc5118783b61bcb770eee14
Shrey <thisisshreykhandelwal@protonmail.com>
2023-01-17 17:06:59 +0530

    Time: 107 ms (79.55%), Space: 10.7 MB (86.09%) - LeetHub

---
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int longestPalindromeSubseq(string x) {
+        
+        string y = x;
+        
+        reverse(y.begin(), y.end());
+        
+        int s1 = x.size();
+        int s2 = y.size();
+        
+        int lcs[s1+1][s2+1];
+        
+        for(int i=0;i<=s1;i++){
+            for(int j=0;j<=s2;j++){
+                if(i==0 || j==0){
+                    lcs[i][j]=0;
+                }
+                else if(x[i-1]==y[j-1]){
+                    lcs[i][j] = 1+lcs[i-1][j-1];
+                }
+                else {
+                    lcs[i][j]=max(lcs[i-1][j], lcs[i][j-1]);
+                }
+            }
+        }
+        return lcs[s1][s2];
+    }
+};
