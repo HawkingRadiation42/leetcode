1eac656e67365744900dfcb1f87e863364b18195
Shrey <thisisshreykhandelwal@protonmail.com>
2023-01-21 14:55:12 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (17.30%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.cpp
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
-    bool checkString(string s) {
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='b'){
-                if(s[i+1]=='a'){
-                    return false;
-                }
-            }
+    // bool checkString(string s) {
+    //     for(int i=0;i<s.size();i++){
+    //         if(s[i]=='b'){
+    //             if(s[i+1]=='a'){
+    //                 return false;
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
+    bool checkString(string s){
+        string str = s;
+        sort(str.begin(), str.end());
+        if(str==s){
+            return true;
         }
-        return true;
+        return false;
     }
 };
