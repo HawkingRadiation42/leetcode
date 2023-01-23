61188ae118cf28b19a47f5831c79ef4ef656656d
Shrey <thisisshreykhandelwal@protonmail.com>
2023-01-23 11:57:18 +0530

    Time: 22 ms (70.09%), Space: 18.1 MB (23.90%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+        int n = nums.size();
+        
+        // if(n==1){
+        //     if(nums[0]==0){
+        //         return 1;
+        //     }
+        //     return 0;
+        // }
+        
+        sort(nums.begin(), nums.end());
+        
+        for(int i=0;i<n;i++){
+            if(nums[i]!=i){
+                // cout<<nums[i];
+                return i;
+            }
+        }
+        return n;
+    }
+};
