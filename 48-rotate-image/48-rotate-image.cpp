761bd04347530ac7b98e6eb89cce4e4876a3710c
Shrey <thisisshreykhandelwal@protonmail.com>
2023-01-25 08:55:46 +0530

    Time: 7 ms (7.55%), Space: 7.2 MB (39.11%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -8,7 +8,12 @@ public:
         }
         int n = matrix.size();
         for(int i = 0;i<n;i++){
-            reverse(matrix[i].begin(), matrix[i].end());
+            int l=0, r=n-1;
+            while(l<r){
+                swap(matrix[i][l], matrix[i][r]);
+                l++;
+                r--;
+            }
         }
     }
 };
