3719736a40835d3459e62a9465dfe5f0a2454698
Shrey <thisisshreykhandelwal@protonmail.com>
2023-01-27 19:28:36 +0530

    Time: 33 ms (43.93%), Space: 19.4 MB (16.95%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i=0, j=0;
-        while(i<nums.size() && j<nums.size()){
-            if(nums[j]!=0){
+        int i=0;
+        int j=1;
+        while(j<nums.size()){
+            if(nums[i]==0 && nums[j]!=0){
                 swap(nums[i], nums[j]);
                 i++;
                 j++;
             }
-            else 
+            else if(nums[i]==0 && nums[j]==0){
+
                 j++;
+            }
+            else{
+                i++;
+                j++;
+            }
         }
     }
 };
