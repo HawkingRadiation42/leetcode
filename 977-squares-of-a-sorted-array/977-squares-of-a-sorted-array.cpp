0666afa3736e1d240cc18184e2118bf601ad8c2c
Shrey <thisisshreykhandelwal@protonmail.com>
2023-01-27 16:27:29 +0530

    Time: 39 ms (71.22%), Space: 25.7 MB (95.32%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
+    
     vector<int> sortedSquares(vector<int>& nums) {
-
-        
-        for(int i=0;i<nums.size();i++){
-            nums[i] = nums[i]*nums[i];
+        vector<int> ans(nums.size());
+        int l =0;
+        int r=nums.size()-1;
+        for(int i=nums.size()-1;i>=0;i--){
+            if(abs(nums[l])<abs(nums[r])){
+                ans[i] = nums[r]*nums[r--];
+                
+            }
+            else
+                ans[i]=nums[l]*nums[l++];
         }
-        sort(nums.begin(), nums.end());
-        return nums;
-        
+        return ans;
     }
 };
