a15d83ccf11d8731c58f4b3610d239031b27f2b6
Shrey <thisisshreykhandelwal@protonmail.com>
2023-01-29 15:30:01 +0530

    Time: 4 ms (37.95%), Space: 6 MB (33.57%) - LeetHub

---
+++ 50-powx-n/50-powx-n.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        
+        if(n==0){
+            return 1;
+        }
+        
+        if(n<0){
+            //code for 1/x^n
+            n=abs(n);
+            x = 1/x;
+        }
+        if(n%2==0){
+            return myPow(x*x, n/2);
+        }
+        else
+            return x*myPow(x*x, n/2);
+    }
+};
