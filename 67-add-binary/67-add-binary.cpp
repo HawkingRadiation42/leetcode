fe2edec006fc870136e9a3f935380c40383671e0
Shrey <thisisshreykhandelwal@protonmail.com>
2023-02-15 11:11:23 +0530

    Time: 4 ms (50.16%), Space: 7.1 MB (12.71%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -1,56 +1,23 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        string ans = "";
-        int n = a.size();
-        int m = b.size();
-        if(abs(n-m)>0){
-            int xtra = abs(n-m);
-            string k="";
-            while(xtra--){
-                k.push_back('0');
-            }
-            if(n>m){
-               k+=b;
-               b=k; 
-            }
-            else {
-                k+=a;
-                a=k;
-            }
-        }
-        int i=a.size()-1;
-        int j=i;
-        int carry=0;
-        while(i>=0){
-            if(a[i]=='1' && b[i]=='1'){
-                if(carry==1){
-                    ans.push_back('1');
-                }
-                else  {
-                    ans.push_back('0');
-                    carry=1;
-                }
-            }
-            else if((a[i]=='0' && b[i]=='1') || (a[i]=='1' && b[i]=='0')){
-                if(carry==1){
-                    ans.push_back('0');
-                }
-                else ans.push_back('1');
-            }
-            else {
-                if(carry==1){
-                    ans.push_back('1');
-                    carry=0;
-                }
-                else ans.push_back('0');
-            }
+        int carry = 0, i = a.size() - 1, j = b.size() - 1;
+    string result = "";
+    
+    while (i >= 0 || j >= 0 || carry != 0) {
+        int sum = carry;
+        if (i >= 0) {
+            sum += a[i] - '0';
             i--;
+        }
+        if (j >= 0) {
+            sum += b[j] - '0';
             j--;
         }
-        if(carry==1) ans.push_back('1');
-        reverse(ans.begin(), ans.end());
-        return ans;
-        
+        result = to_string(sum % 2) + result;
+        carry = sum / 2;
+    }
+    
+    return result;
     }
 };
