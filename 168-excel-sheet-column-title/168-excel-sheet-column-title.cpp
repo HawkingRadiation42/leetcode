f35e013ae36ff9bea057d90eef616124d8ac412d
Shrey <thisisshreykhandelwal@protonmail.com>
2023-03-11 18:41:26 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (81.10%) - LeetHub

---
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string column = "";
+        int rem;
+
+        while(columnNumber){
+            rem = (--columnNumber)%26;  // similar to subtracting 1 from both sides and obtaining the remainder
+            columnNumber /= 26;
+
+            column = char('A' + rem) + column;
+        }
+
+        return column;
+    }
+};
