b4c9b30b0e2e80cd3a8f6b1929069d2b25b6ea7e
Shrey Khandelwal <thisisshreykhandelwal@protonmail.com>
2023-03-29 21:56:05 +0530

    Time: 3 ms (57.55%), Space: 10 MB (51.86%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -11,22 +11,14 @@
  */
 class Solution {
 public:
-
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        // If both are NULL
-        if(p == NULL && q == NULL){
+        if(!p && !q)
             return true;
-        }
-        // If one is NULL and other is not NULL
-        // If both NULL (above will return true);
-        if(p == NULL || q == NULL){
+        if(!p || !q)
             return false;
-        }
-        // Comparing values.
-        if(p->val != q->val){
+        if(q->val != p->val)
             return false;
-        }
-        // Go to left and right subtree.
-        return(isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
+   
+        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);;
     }
 };
