350e1176f687cb0dc9b0398266903a3bd2b96ef9
Shrey Khandelwal <thisisshreykhandelwal@protonmail.com>
2023-04-02 20:38:18 +0530

    Time: 39 ms (69.15%), Space: 36.2 MB (73.59%) - LeetHub

---
+++ 485-max-consecutive-ones/485-max-consecutive-ones.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count =0, maxcount=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=1){
+                maxcount = max(count, maxcount);
+                count = 0;
+            }
+            else{
+                count++;
+                maxcount = max(count, maxcount);
+            }
+        }
+        return maxcount;
+    }
+};
