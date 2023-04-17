104eca71e663815d047d9202b78aa7a27173fe5c
Shrey Khandelwal <thisisshreykhandelwal@protonmail.com>
2023-04-18 01:44:49 +0530

    Time: 18 ms (32.74%), Space: 14.4 MB (74.75%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -4,10 +4,28 @@ public:
         if(target == arr){
             return true;
         }
-        sort(target.begin(), target.end());
-        sort(arr.begin(), arr.end());
-        for(int i=0;i<target.size();i++){
-            if(target[i] != arr[i]){
+        if (target.size() != arr.size()) {
+            return false;
+        }
+//      brute force approach 
+        
+        // sort(target.begin(), target.end());
+        // sort(arr.begin(), arr.end());
+        // for(int i=0;i<target.size();i++){
+        //     if(target[i] != arr[i]){
+        //         return false;
+        //     }
+        // }
+        // return true;
+        
+//         smart approach only n complexity instead of nlogn
+        int count[1001] = {0};
+        for(int i =0;i<arr.size();i++){
+            count[target[i]]++;
+            count[arr[i]]--;
+        }
+        for(int i=0;i<1001;i++){
+            if (count[i] != 0) {
                 return false;
             }
         }
