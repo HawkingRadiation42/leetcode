bb8d25c3986e08339c2649f4be0882d043ab7d74
Shrey Khandelwal <thisisshreykhandelwal@protonmail.com>
2023-04-18 01:15:51 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (71.01%) - LeetHub

--- 1103-distribute-candies-to-people/1103-distribute-candies-to-people.cpp
+++ 1103-distribute-candies-to-people/1103-distribute-candies-to-people.cpp
@@ -3,10 +3,12 @@ public:
     vector<int> distributeCandies(int candies, int num_people) {
         vector<int> res(num_people, 0);
         int i=0;
+        //  concept is to run a while loop till the candies arent finished 
+//         the number of candies assigned is calculated using whichever is minimum remaining candies or i+1 which is to be distributed 
         while(candies>0){
             int candies_given = min(candies, i+1);
-            res[i%num_people] += candies_given;
-            candies -= candies_given;
+            res[i%num_people] += candies_given; // here we using modular for iteration eg 012, 012 again and agin for increasing value of i 
+            candies -= candies_given; // deduct the candies assigned to the person
             i++;
         }
         return res;
