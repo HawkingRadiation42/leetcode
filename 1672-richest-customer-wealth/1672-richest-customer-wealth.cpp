b0f6e4f05c55bbcb08cace32d81a2842d4030e91
Shrey Khandelwal <thisisshreykhandelwal@protonmail.com>
2023-04-18 01:21:56 +0530

    Time: 9 ms (27.68%), Space: 7.8 MB (61.74%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
-        int person=0;
+        int global=0;
         int add=0;
         for(int i=0;i<accounts.size();i++){
             add = 0;
             for(int j=0;j<accounts[0].size();j++){
                 add += accounts[i][j];
             }
-            person = max(add, person);
+            global = max(add, global);
         }
-        return person;
+        return global;
     }
 };
