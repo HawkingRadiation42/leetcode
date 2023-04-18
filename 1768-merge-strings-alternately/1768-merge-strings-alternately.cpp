5b90382ab66d247e1b976440bfa5e681fb81fdb0
Shrey Khandelwal <thisisshreykhandelwal@protonmail.com>
2023-04-18 08:42:53 +0530

    Time: 5 ms (10.49%), Space: 9.3 MB (5.19%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -2,34 +2,17 @@ class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
         string res;
-        if(word1.size()==word2.size()){
-            for(int i=0;i<word1.size();i++){
-                res = res+word1[i];
-                res = res+word2[i];
+        int p=word1.size();
+        int q=word2.size();
+        int n=max(p,q);
+        for(int i=0;i<n;i++){
+            if(p>0){
+                res=res+word1[i];
+                p--;
             }
-        }
-        else if(word1.size()>word2.size()){
-            for(int i=0;i<word2.size();i++){
-                res = res+word1[i];
-                res = res+word2[i];
-            }
-            int i = word1.size()-word2.size();
-            int j=0;
-            while(i>j){
-                res+= word1[word2.size()+j];
-                j++;
-            }
-        }
-        else if(word1.size()<word2.size()){
-            for(int i=0;i<word1.size();i++){
-                res = res+word1[i];
-                res = res+word2[i];
-            }
-            int i = word2.size()-word1.size();
-            int j=0;
-            while(i>j){
-                res+= word2[word1.size()+j];
-                j++;
+            if(q>0){
+                res=res+word2[i];
+                q--;
             }
         }
         return res;
