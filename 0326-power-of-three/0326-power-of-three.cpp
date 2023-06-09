49cc5f444d9c4585b8c11d89c59406f78aef9e3c
Shrey Khandelwal <thisisshreykhandelwal@protonmail.com>
2023-06-09 12:30:06 +0530

    Time: 20 ms (77.48%), Space: 6.1 MB (21.65%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n<=0){
+        if (n <= 0) {
             return false;
         }
-        while(n>1){
-            if(n%3 != 0){
-                return false;
-            }
-            n/=3;
+
+        double logvalue = log10(n) / log10(3);
+        if(logvalue-int(logvalue)==0){
+            return true;
         }
-        return true;
+        //first the logvalue is kept as double to check for the logvalue if it is in decimal coz if its in decimal then its not power of three which we check if the decimal log value is same as integer logvalue
+        return false;
     }
 };
