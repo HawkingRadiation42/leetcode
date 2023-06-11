1c41a4d249e39486e063969f4144ccd57a1adc9d
Shrey Khandelwal <thisisshreykhandelwal@protonmail.com>
2023-06-11 10:54:55 +0530

    Time: 446 ms (24.02%), Space: 156.1 MB (26.34%) - LeetHub

---
+++ 1146-snapshot-array/1146-snapshot-array.cpp
@@ -0,0 +1,28 @@
+class SnapshotArray {
+public:
+    vector<map<int,int>> a;
+    int s_id = 0;
+
+    SnapshotArray(int length) {
+        a.resize(length);
+        for(int i = 0; i < length; i++)
+            a[i][0] = 0;
+        
+    }
+    void set(int index, int val) {
+        a[index][s_id] = val; // updates them in accordance to the current snap_id
+    }
+    int snap() {
+        s_id++;
+        return  s_id - 1;
+    }
+    int get(int index, int snap_id) {
+        if(a[index].find(snap_id) == a[index].end())
+        {
+            auto it = --a[index].lower_bound(snap_id);
+            return it->second;
+        }
+        return a[index][snap_id];
+    }
+
+};
