69daff54dcf9842419036176ec4383a496acf3c7
Shrey Khandelwal <thisisshreykhandelwal@protonmail.com>
2023-06-19 15:06:47 +0530

    Time: 3 ms (50.72%), Space: 8.1 MB (20.89%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    
+    int largestAltitude(vector<int>& gain) {
+    vector<int> res(gain.size() + 1);  // Initialize `res` with the appropriate size
+    res[0] = 0;
+    for (int i = 1; i < res.size(); i++) {
+        res[i] = res[i - 1] + gain[i - 1];  // Calculate the cumulative altitude
+    }
+    
+    int maxi = 0;
+    for (int i = 0; i < res.size(); i++) {
+        maxi = max(maxi, res[i]);  // Find the highest altitude
+    }
+    
+    return maxi;
+}
+
+
+};
