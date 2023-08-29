e7e96e9b60366575fbfa4a0d70b9d3b5e56b07e6
Shrey Khandelwal <thisisshreykhandelwal@protonmail.com>
2023-08-29 23:41:38 +0530

    Time: 22 ms (85.33%), Space: 10.6 MB (86.10%) - LeetHub

---
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    
+    int bestClosingTime(string cust) {
+        int p = count(begin(cust), end(cust), 'Y');
+        int min_p = p, j = 0;
+        for (int i = 0; i < cust.size(); ++i) {
+            p += (cust[i] == 'Y') ? -1 : 1;
+            if (p < min_p) {
+                min_p = p;
+                j = i + 1;
+            }
+        }
+        return j;
+    }
+
+};
